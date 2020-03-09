1cc9977bc059455710297411b66b5f6035853a9e
STSchiff <chrisheib@gmx.de>
2020-03-09 18:01:27 +0100

    Send Content Length

--- MPS.main.go
+++ MPS.main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // https://jaxenter.de/restful-rest-api-go-golang-68845
 func main() {
 	http.HandleFunc("/", productsHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 }
 
 // https://stackoverflow.com/questions/24116147/how-to-download-file-in-browser-from-go-serverv
@@ -22,7 +23,10 @@ func productsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Print(err)
 	}
+	var stat, _ = fileContent.Stat()
+	var size = stat.Size()
 	w.Header().Set("Content-Disposition", "attachment; filename=music.mp3")
 	w.Header().Set("Content-Type", "audio/mpeg")
+	w.Header().Set("Content-Length", strconv.FormatInt(size, 10))
 	io.Copy(w, fileContent)
 }
